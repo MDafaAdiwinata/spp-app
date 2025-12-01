ca89ccb40b785e7b70845078c987fec78d66b186
MDafaAdiwinata <dafaadi2106@gmail.com>
2025-12-01 21:21:02 +0700

    Add syntax db connection railway

--- spp-app/database.js
+++ spp-app/database.js
@@ -1,10 +1,12 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "ukk_spp"
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: Number(process.env.MYSQLPORT),
+  ssl: { rejectUnauthorized: false }
 });
 
 db.connect((err) => {
